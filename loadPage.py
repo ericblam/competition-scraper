6c1b884a7ddfd46036b6e048f305c8feab46b4bc
Eric Lam <marcusgladir@gmail.com>
2017-09-18 22:17:28 -0400

    Add functionality to cache pages

--- loadPage.py
+++ loadPage.py
@@ -1,24 +1,49 @@
+import os
 import urllib.error, urllib.parse, urllib.request
 from bs4 import BeautifulSoup
 from tidylib import tidy_document
+
+CACHE_DIR = ".cache/"
 
 """
 Loads a page from a url with data (uses GET if !post, else uses POST)
 """
 def loadPage(url, data={}, post=False):
     request = urllib.parse.urlencode(data)
+
+    sortedDataString = "_".join([str(i) + "=" + str(data[i]) for i in sorted(data.keys())])
+    cacheFilename = url
+    if (len(sortedDataString) > 0):
+        cacheFilename += "_" + ("POST" if post else "GET") + "_" + sortedDataString
+
+    cacheFilename = cacheFilename.replace("http://", "").replace("/", "_")
+    if (not os.path.exists(CACHE_DIR)):
+        os.makedirs(CACHE_DIR)
+
+    cacheFilename = CACHE_DIR + cacheFilename
+    if (os.path.exists(cacheFilename)):
+        cachedPage = open(cacheFilename)
+        tidiedPage = cachedPage.read()
+        cachedPage.close()
+        return BeautifulSoup(tidiedPage, "html.parser")
+
     try:
         if (not post):
-            getUrl = url + ('?' if len(data) != 0 else '') + request
+            getUrl = url + ("?" if len(data) != 0 else "") + request
             response = urllib.request.urlopen(getUrl)
         else:
-            response = urllib.request.urlopen(url, request.encode('ascii'))
+            response = urllib.request.urlopen(url, request.encode("ascii"))
         html_response = response.read()
-        encoding = response.headers.get_content_charset('utf-8')
+        encoding = response.headers.get_content_charset("utf-8")
         decoded_html = html_response.decode(encoding)
         tidiedPage, pageErrors = tidy_document(decoded_html)
-        return BeautifulSoup(tidiedPage, 'html.parser')
+
+        cachedPage = open(cacheFilename, "w+")
+        cachedPage.write(tidiedPage)
+        cachedPage.close()
+
+        return BeautifulSoup(tidiedPage, "html.parser")
     except urllib.error.HTTPError:
         print("Failed to fetch %s with request: %s" % (url, request))
         return None
-    # return BeautifulSoup(u, 'html.parser')
+    # return BeautifulSoup(u, "html.parser")
