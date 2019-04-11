b0469c71d1915dab4a0738b318d250c5dbcc8323
Eric Lam <marcusgladir@gmail.com>
2019-04-10 21:58:16 -0400

    Add script to help develop individual parsers

---
+++ src/crawler_tester.py
@@ -0,0 +1,62 @@
+import json
+import queue
+import sys
+
+from db import dbaccessor
+from webparser import webparser, parsertype
+from util.crawlerutils import ScraperTask
+from util.webutils import WebRequest, loadPage
+
+if __name__ == "__main__":
+
+    if len(sys.argv) < 2:
+        print("Usage: python3 %s <config-file>" % sys.argv[0])
+        exit()
+
+    # mock setup
+    config = {}
+    with open(sys.argv[1]) as configFile:
+        config = json.load(configFile)
+
+    q = queue.Queue()
+    conn = dbaccessor.createConn(config['db'])
+
+    url = "http://results.o2cm.com/scoresheet3.asp"
+    compId = "rub19"
+    heatId = "40323030"
+    roundNum = 0
+
+    # set up data/requests/tasks
+    requestData = {
+        'event': compId,
+        'heatId': heatId,
+        'selCount': roundNum
+    }
+    nextRequest = WebRequest(url, "POST", requestData)
+
+    newData = {
+        'compId': compId,
+        'heatId': heatId,
+        'roundNum': roundNum
+    }
+
+    task = ScraperTask(
+        nextRequest,
+        newData,
+        parsertype.ParserType.O2CM_HEAT)
+
+    ############################# actual crawling
+
+    # get HTML
+    htmlDOM = loadPage(task.request)
+
+    # if no hint, need to create one
+    if task.hint is None:
+        task.hint = webparser.getParserHint(task.request)
+
+    # determine how to parse HTML
+    parser = webparser.O2cmHeatParser(q, conn, config)
+
+    # parse HTML
+    if parser is not None:
+        parser.parse(htmlDOM, task.data)
